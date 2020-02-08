e9f1d5955a8b0189e22ebea6de1d12d2f3c197cd
Miguel Rivas Perez <mrivasperez1996@gmail.com>
2020-02-07 21:11:12 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,124 +1,66 @@
-//created the variable colors by calling the function generateRandomColors to call the initial 6 colors for the game
 var colors = generateRandomColors(6);
-
-//create variable for numSquares to 6. this variable will be called later during future calls to other functions.
 var numSquares = 6;
-
-//select and create variable for the squares
 var squares = document.querySelectorAll(".square");
-
-//select and create variable for picked color by calling the pickColor function
 var pickedColor = pickColor();
-
-//select and create variable for colorDisplay using getElementByID so that the HTML element is changed
 var colorDisplay = document.getElementById('colorDisplay');
-
-//select element with ID of message 
 var messageDisplay = document.querySelector("#message");
-
-//select and create variable for  h1 in index.html by using querySelector to select element with class of heading
 var h1 = document.querySelector('.heading');
-
-//select and create variable for reset button by using query selector to select element with ID of reset
 var resetBtn = document.querySelector("#reset");
-
-//select and create variable for the easy button by using query selector to select element with ID of easyBtn
 var easyBtn = document.querySelector("#easyBtn");
-
-//select the hard button
 var hardBtn = document.querySelector("#hardBtn");
 
-//easy button - begin by create an event listener for click that runs the following function
 easyBtn.addEventListener("click", function(){
-    //add the class selected to easyBtn so that the appearance of the easyBtn changes based on css file.
+   
     easyBtn.classList.add("selected");
-
-    ///remove the class selected from hardBtn so that the appearance of the easyBtn changes based on css file.
     hardBtn.classList.remove("selected");
-
-    //change numSquares to be 3 so that it displays an easier grid of 3 instead of 6
-    numSquares = 3
-
-    //generate a new set of random colors
+    numSquares = 3;
     colors = generateRandomColors(numSquares);
-
-    //generate a new pickColor
     pickedColor = pickColor();
-
-    //get new colors, remove the 3 squares under the new 3 squares
     for(var i = 0; i < squares.length; i++){
         if(colors[i]){
             squares[i].style.background = colors[i];
         } else {
             squares[i].style.display = "none";
         }
-    }
-
-    //revert the h1 background to the initial style
+    };
     h1.style.backgroundImage = "linear-gradient(315deg, #485461 0%, #28313b 74%)";
-
-    //remove mesage
     messageDisplay.textContent = "";
-
-    //make sure colorDisplay shows new picked color
     colorText();
+
 })
 
-//hard button - begin by create an event listener for click that runs the following function
 hardBtn.addEventListener("click", function(){
-    //add the class selected to hardBtn so that the appearance of the easyBtn changes based on css file.
+    
     hardBtn.classList.add("selected");
-
-    ///remove the class selected from hardBtn so that the appearance of the easyBtn changes based on css file.
-    easyBtn.classList.remove("selected");
-
-    //change numSquares to be 6 so that it displays regular grid of 6    
+    easyBtn.classList.remove("selected"); 
     numSquares = 6
-
-    //generate a new set of random colors
-    colors = generateRandomColors(numSquares);
-
-    //generate a new pickColor    
+    colors = generateRandomColors(numSquares);  
     pickedColor = pickColor();
-
-
     colorDisplay.textContent = pickedColor;
 
-    //get new colors, make all 6 squares display in case the player had previously selected an easy game.   
     for(var i = 0; i < squares.length; i++){
         squares[i].style.background = colors[i];
         squares[i].style.display = "block";
     }
 
-    //revert the h1 background to the initial style
     h1.style.backgroundImage = "linear-gradient(315deg, #485461 0%, #28313b 74%)";
 
-    //remove mesage
     messageDisplay.textContent = "";
 
-    //make sure colorDisplay shows new picked color
     colorText();
 })
 
-//reset button
 resetBtn.addEventListener("click", function(){
 
-    //generate all new colors
     colors = generateRandomColors(numSquares);
-
-    //pick a new random color from array
     pickedColor = pickColor();
     
-    //get new colors
     for(var i = 0; i < squares.length; i++){
         squares[i].style.background = colors[i]
     } 
+
     h1.style.backgroundImage = "linear-gradient(315deg, #485461 0%, #28313b 74%)";
-
-    //remove messageDisplay text
     messageDisplay.textContent = "";
-
-    //make sure colorDisplay shows new picked color
     colorText();
 
 })
